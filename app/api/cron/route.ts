5010158d8ad9e282d71eb1d991b7cee4b24824f3
Your Name <you@example.com>
2023-11-20 00:59:07 +0530

    trying the same thing again

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -1,12 +1,12 @@
 import { NextResponse } from "next/server";
 
-import { getLowestPrice,getHighestPrice,getAveragePrice,getEmailNotifType } from "@/lib/ulits";
+import { getLowestPrice, getHighestPrice, getAveragePrice, getEmailNotifType } from "@/lib/ulits";
 import { connectToDB } from "@/lib/mongoose";
 import Product from "@/lib/models/product.model";
 import { scrapeAmazonProduct } from "@/lib/scraper";
 import { generateEmailBody, sendEmail } from "@/lib/nodemailer";
 
- 
+export const maxDuration = 300; // This function can run for a maximum of 300 seconds
 export const dynamic = "force-dynamic";
 export const revalidate = 0;
 
